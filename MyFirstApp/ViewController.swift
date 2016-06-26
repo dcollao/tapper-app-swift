f55f7e5740c8f0deb3cde37a31563f26d0a0d1ee
Diego  Collao <dcollao@icloud.com>
2016-06-26 04:26:22 -0400

    pio

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,17 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Default properties
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    //All var are declared.
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var tapTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapLbl: UILabel!
+    
+    @IBAction func onTapBtnPressed(sender: UIButton!){
+        currentTaps += 1
+        updateTapsLbl()
+        
+        if isGameOver(){
+            restartGame()
+        }
+    }
+    
+    //So I can declare actions like:
+    @IBAction func onPlayBtnPressed(sender: UIButton!){
+        if tapTxt.text != nil && tapTxt.text != "" {
+            //Hidden
+            logoImg.isHidden = true
+            tapTxt.isHidden = true
+            playBtn.isHidden = true
+            //Un-hidden
+            tapBtn.isHidden = false
+            tapLbl.isHidden = false
+            maxTaps = Int(tapTxt.text!)!
+            updateTapsLbl()
+        }
+    }
+    
+    func restartGame() {
+        maxTaps = 0
+        tapTxt.text = ""
+        
+        logoImg.isHidden = false
+        tapTxt.isHidden = false
+        playBtn.isHidden = false
+        
+        tapBtn.isHidden = true
+        tapLbl.isHidden = true
+    }
+    
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
+    func updateTapsLbl(){
+        tapLbl.text = "\(currentTaps) Taps"
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tapTxt.keyboardType = .numberPad
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
